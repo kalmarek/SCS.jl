bd767bf63e9f42b0676bb648e4e9e565eef44727
kalmarek <kalmar@amu.edu.pl>
2019-06-03 13:51:38 +0200

    allow different scsint_t definitions on the c-side

--- src/types.jl
+++ src/types.jl
@@ -4,31 +4,34 @@ export SCSMatrix, SCSData, SCSSettings, SCSSolution, SCSInfo, SCSCone, SCSVecOrM
 
 SCSVecOrMatOrSparse = Union{VecOrMat, SparseMatrixCSC{Float64,Int}}
 
-struct SCSMatrix
+SCSInt = Union{Int64, Int32}
+
+
+struct SCSMatrix{T <: SCSInt}
     values::Ptr{Cdouble}
-    rowval::Ptr{Int}
-    colptr::Ptr{Int}
-    m::Int
-    n::Int
+    rowval::Ptr{T}
+    colptr::Ptr{T}
+    m::T
+    n::T
 end
 
 # Version where Julia manages the memory for the vectors.
-struct ManagedSCSMatrix
+struct ManagedSCSMatrix{T <: SCSInt}
     values::Vector{Cdouble}
-    rowval::Vector{Int}
-    colptr::Vector{Int}
-    m::Int
-    n::Int
+    rowval::Vector{T}
+    colptr::Vector{T}
+    m::T
+    n::T
 end
 
-function ManagedSCSMatrix(m::Int, n::Int, A::SCSVecOrMatOrSparse)
+function ManagedSCSMatrix(m::T, n::T, A::SCSVecOrMatOrSparse) where T
     A_sparse = sparse(A)
 
     values = copy(A_sparse.nzval)
-    rowval = convert(Array{Int, 1}, A_sparse.rowval .- 1)
-    colptr = convert(Array{Int, 1}, A_sparse.colptr .- 1)
+    rowval = A_sparse.rowval .- 1
+    colptr = A_sparse.colptr .- 1
 
-    return ManagedSCSMatrix(values, rowval, colptr, m, n)
+    return ManagedSCSMatrix{T}(values, rowval, colptr, m, n)
 end
 
 # Returns an SCSMatrix. The vectors are *not* GC tracked in the struct.
@@ -37,27 +40,25 @@ SCSMatrix(m::ManagedSCSMatrix) =
     SCSMatrix(pointer(m.values), pointer(m.rowval), pointer(m.colptr), m.m, m.n)
 
 
-struct SCSSettings
-    normalize::Int # boolean, heuristic data rescaling
+struct SCSSettings{T <: SCSInt}
+    normalize::T # boolean, heuristic data rescaling
     scale::Cdouble # if normalized, rescales by this factor
     rho_x::Cdouble # x equality constraint scaling
-    max_iters::Int # maximum iterations to take
+    max_iters::T # maximum iterations to take
     eps::Cdouble # convergence tolerance
     alpha::Cdouble # relaxation parameter
     cg_rate::Cdouble # for indirect, tolerance goes down like (1/iter)^cg_rate
-    verbose::Int # boolean, write out progress
-    warm_start::Int # boolean, warm start (put initial guess in Sol struct)
-    acceleration_lookback::Int # acceleration memory parameter
+    verbose::T # boolean, write out progress
+    warm_start::T # boolean, warm start (put initial guess in Sol struct)
+    acceleration_lookback::T # acceleration memory parameter
     write_data_filename::Cstring
 
-    SCSSettings() = new()
-    SCSSettings(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose, warm_start, acceleration_lookback, write_data_filename) = new(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose, warm_start, acceleration_lookback, write_data_filename)
+    SCSSettings{T}() where T = new{T}()
+    SCSSettings{T}(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose, warm_start, acceleration_lookback) where T =
+    new{T}(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose, warm_start, acceleration_lookback, write_data_filename)
 end
 
-struct Direct end
-struct Indirect end
-
-function _SCS_user_settings(default_settings::SCSSettings;
+function _SCS_user_settings(default_settings::SCSSettings{T};
         normalize=default_settings.normalize,
         scale=default_settings.scale,
         rho_x=default_settings.rho_x,
@@ -68,32 +69,32 @@ function _SCS_user_settings(default_settings::SCSSettings;
         verbose=default_settings.verbose,
         warm_start=default_settings.warm_start,
         acceleration_lookback=default_settings.acceleration_lookback,
-        write_data_filename=default_settings.write_data_filename
-        )
-    return SCSSettings(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose,warm_start, acceleration_lookback, write_data_filename)
+        write_data_filename=default_settings.write_data_filename) where T
+    return SCSSettings{T}(normalize, scale, rho_x, max_iters, eps, alpha, cg_rate, verbose,warm_start, acceleration_lookback, write_data_filename)
 end
 
 function SCSSettings(linear_solver::Union{Type{Direct}, Type{Indirect}}; options...)
 
     mmatrix = ManagedSCSMatrix(0,0,spzeros(1,1))
     matrix = Ref(SCSMatrix(mmatrix))
-    default_settings = Ref(SCSSettings())
-    dummy_data = Ref(SCSData(0,0, Base.unsafe_convert(Ptr{SCSMatrix}, matrix),
+    default_settings = Ref(SCSSettings{IntT}())
+    dummy_data = Ref(SCSData{IntT}(0, 0,
+        Base.unsafe_convert(Ptr{SCSMatrix{IntT}}, matrix),
         pointer([0.0]), pointer([0.0]),
         Base.unsafe_convert(Ptr{SCSSettings}, default_settings)))
     SCS_set_default_settings(linear_solver, dummy_data)
     return _SCS_user_settings(default_settings[]; options...)
 end
 
-struct SCSData
+struct SCSData{T <: SCSInt}
     # A has m rows, n cols
-    m::Int
-    n::Int
-    A::Ptr{SCSMatrix}
+    m::T
+    n::T
+    A::Ptr{SCSMatrix{T}}
     # b is of size m, c is of size n
     b::Ptr{Cdouble}
     c::Ptr{Cdouble}
-    stgs::Ptr{SCSSettings}
+    stgs::Ptr{SCSSettings{T}}
 end
 
 struct SCSSolution
@@ -102,10 +103,11 @@ struct SCSSolution
     s::Ptr{Nothing}
 end
 
-struct SCSInfo
-    iter::Int
+
+struct SCSInfo{T <: SCSInt}
+    iter::T
     status::NTuple{32, Cchar} # char status[32]
-    statusVal::Int
+    statusVal::T
     pobj::Cdouble
     dobj::Cdouble
     resPri::Cdouble
@@ -117,7 +119,7 @@ struct SCSInfo
     solveTime::Cdouble
 end
 
-SCSInfo() = SCSInfo(0, ntuple(_ -> zero(Cchar), 32), 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+SCSInfo(::Type{T}) where T = SCSInfo{T}(0, ntuple(_ -> zero(Cchar), 32), 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
 function raw_status(info::SCSInfo)
     s = collect(info.status)
@@ -139,24 +141,24 @@ end
 # dual exponential cones {(u,v,w) | −u e^(v/u) <= e w, u<0}
 # power cones {(x,y,z) | x^a * y^(1-a) >= |z|, x>=0, y>=0}
 # dual power cones {(u,v,w) | (u/a)^a * (v/(1-a))^(1-a) >= |w|, u>=0, v>=0}
-struct SCSCone
-    f::Int # number of linear equality constraints
-    l::Int # length of LP cone
-    q::Ptr{Int} # array of second-order cone constraints
-    qsize::Int # length of SOC array
-    s::Ptr{Int} # array of SD constraints
-    ssize::Int # length of SD array
-    ep::Int # number of primal exponential cone triples
-    ed::Int # number of dual exponential cone triples
+struct SCSCone{T <: SCSInt}
+    f::T # number of linear equality constraints
+    l::T # length of LP cone
+    q::Ptr{T} # array of second-order cone constraints
+    qsize::T # length of SOC array
+    s::Ptr{T} # array of SD constraints
+    ssize::T # length of SD array
+    ep::T # number of primal exponential cone triples
+    ed::T # number of dual exponential cone triples
     p::Ptr{Cdouble} # array of power cone params, must be \in [-1, 1], negative values are interpreted as specifying the dual cone
-    psize::Int # length of power cone array
+    psize::T # length of power cone array
 end
 
 # Returns an SCSCone. The q, s, and p arrays are *not* GC tracked in the
 # struct. Use this only when you know that q, s, and p will outlive the struct.
-function SCSCone(f::Int, l::Int, q::Vector{Int}, s::Vector{Int},
-                 ep::Int, ed::Int, p::Vector{Cdouble})
-    return SCSCone(f, l, pointer(q), length(q), pointer(s), length(s), ep, ed, pointer(p), length(p))
+function SCSCone(f::T, l::T, q::Vector{T}, s::Vector{T},
+                 ep::T, ed::T, p::Vector{Cdouble}) where T
+    return SCSCone{T}(f, l, pointer(q), length(q), pointer(s), length(s), ep, ed, pointer(p), length(p))
 end
 
 
